cd02cc700a4deadcbfd6707aa3f0b2f9c4f9c4b4
tein1142 <tein1142@hotmail.com>
2021-05-09 06:18:03 +0700

    [ADD] Deploy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -73,7 +73,7 @@ export default {
     
     async fetchProductResult() {
       try {
-        const res = await fetch("http://172.22.0.3:3000/products/items");
+        const res = await fetch("http://172.22.0.3:8080/products/items");
         const data = await res.json();
         return data;
       } catch (error) {
